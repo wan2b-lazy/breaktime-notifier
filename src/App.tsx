43efdf1e1664aa104940f40e34904d1254be3c6f
satsuka <wan2b.lazy@gmail.com>
2023-07-17 00:20:21 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-function App() {
+import "./App.css";
+import Home from "./components/Home";
+import Ongoing from "./components/Ongoing";
+import Notify from "./components/Notify";
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/ongoing" element={<Ongoing />} />
+        <Route path="/notify" element={<Notify />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
+
