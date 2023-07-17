5d9a7fc19a032412914c22d93c8b536213af3f2e
satsuka <wan2b.lazy@gmail.com>
2023-07-17 16:20:18 +0900

    Homeコンポーネントの見た目が大体完成

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,60 @@
 import React from "react";
+import {
+  Container,
+  Box,
+  Stack,
+  Typography,
+  TextField,
+  Button,
+  InputAdornment,
+} from "@mui/material";
 
 const Home: React.FC = () => {
-  return <div></div>;
+  return (
+    <Container maxWidth="xs">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography component="h1" variant="h4">
+          アラーム設定
+        </Typography>
+
+        <Box component="form" sx={{ mt: 6 }}>
+          {/* TODO 半角英数字、1以上300以下に制限するバリデーションを追加する */}
+          <TextField
+            id="interval"
+            label="間隔"
+            fullWidth
+            margin="normal"
+            InputProps={{
+              endAdornment: <InputAdornment position="end">分</InputAdornment>,
+              defaultValue: 60,
+            }}
+          />
+          <TextField
+            id="closingTime"
+            label="終了予定時刻"
+            type="time"
+            fullWidth
+            margin="normal"
+          />
+          <Button
+            type="button"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            作業開始
+          </Button>
+        </Box>
+      </Box>
+    </Container>
+  );
 };
 
 export default Home;
